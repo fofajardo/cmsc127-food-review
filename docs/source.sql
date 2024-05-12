bc18fb07058f251551cad414f2f32bae3728de02
fofajardo <fofajardo@up.edu.ph>
2024-05-12 17:53:12 +0800

    feat: integrate DML queries for remaining CRUD operations from scratch.sql.

--- docs/source.sql
+++ docs/source.sql
@@ -300,3 +300,132 @@ INSERT INTO review (type, note, date, rating, userid, foodestid, fooditemid) VAL
         (SELECT foodestid FROM fooditem WHERE name = "Caesar Salad"),
         (SELECT fooditemid FROM fooditem WHERE name = "Caesar Salad")
     );
+
+-- FEATURES
+-- 1. Add, update, and delete a food review (on a food establishment or a food item)
+    -- a. Add a review
+    INSERT INTO review (type, note, date, rating, userid, foodestid, fooditemid)
+    VALUES (
+        "food_item",
+        "This is a negative food item review.",
+        "2024-01-25",
+        1.0,
+        (SELECT userid FROM user WHERE username = "jpr1890"),
+        (SELECT foodestid FROM fooditem WHERE name = "Caesar Salad"),
+        (SELECT fooditemid FROM fooditem WHERE name = "Caesar Salad")
+    );
+    -- b. Update a review
+    UPDATE review -- only details can be updated
+    SET note = "This is a positive food item review.", date = DATE(NOW()), rating = 4.0
+    WHERE reviewid = 16;
+    -- c. Delete a review
+    DELETE FROM review WHERE reviewid = 16;
+
+-- 2. Add, delete, search, and update a food establishment;
+    -- a. Add foodestablishment
+    INSERT INTO foodestablishment (location, name, userid)
+    VALUES ("location", "name", 2);
+    -- b. Update foodestablishment
+    UPDATE foodestablishment -- only details can be updated
+    SET location = "location", name = "unnamed restaurant"
+    WHERE foodestid = 6;
+    -- c. Search foodestablishment
+        -- i. Get all details of food establishment given id
+        SELECT *
+        FROM foodestablishment
+        WHERE foodestid = 6;
+        -- ii. Search by location
+        SELECT foodestid
+        FROM foodestablishment
+        WHERE location = "location";
+        -- iii. Search by name
+        SELECT foodestid
+        FROM foodestablishment
+        WHERE name LIKE "%name%";
+        -- iv. Search by average rating (minimum)
+        SELECT foodestid
+        FROM ratedfoodestablishment
+        WHERE average_rating >= 1.1;
+        -- v. Search by average rating (range)
+        SELECT foodestid
+        FROM ratedfoodestablishment
+        WHERE average_rating >= 1 AND average_rating <= 5;
+    -- d. Delete foodestablishment
+    DELETE FROM foodestablishment WHERE foodestid = 6;
+
+-- 3. Add, delete, search, and update a food item.
+    -- a. Add fooditem
+    INSERT INTO fooditem (name, price, userid, foodestid)
+    VALUES ("name", 1.1, 2, 2);
+    -- b. Update fooditem
+    UPDATE fooditem -- only details can be updated
+    SET name = "unnamed food item", price = 1.1
+    WHERE fooditemid = 26;
+    -- c. Search fooditem
+        -- i. Get all details of food item given id
+        SELECT *
+        FROM fooditem
+        WHERE fooditemid = 26;
+        -- ii. Search by name
+        SELECT fooditemid
+        FROM fooditem
+        WHERE name LIKE "%name%";
+        -- iii. Search by price (minimum)
+        SELECT fooditemid
+        FROM fooditem
+        WHERE price >= 0;
+        -- iv. Search by price (range)
+        SELECT fooditemid
+        FROM fooditem
+        WHERE price >= 0 AND price <= 1000;
+    -- d. Delete fooditem
+    DELETE FROM fooditem
+    WHERE fooditemid = 26;
+
+-- REPORTS TO BE GENERATED
+-- 1. View all food establishments;
+    SELECT * FROM foodestablishment;
+-- 2. View all food reviews for an establishment or a food item;
+    SELECT * FROM review
+    WHERE foodestid = 1
+    AND fooditemid IS NOT NULL; -- review of food items of the establishment
+-- 3. View all food items from an establishment;
+    SELECT * FROM fooditem
+    WHERE foodestid = 1;
+-- 4. View all food items from an establishment that belong to a food type {meat | veg | etc.};
+    SELECT * FROM fooditem AS i NATURAL JOIN foodtype AS t
+    WHERE i.foodestid = 1
+    AND t.type IN ("BBQ", "Burger");
+-- 5. View all reviews made within a month for an establishment or a food item;
+    -- a. Get all reviews made within a month for an establishment
+    SELECT * FROM review
+    WHERE foodestid = 4
+    AND date LIKE "____-10-%";
+    -- b. Get all reviews made within a month for a food item
+    SELECT * FROM review
+    WHERE fooditemid = (SELECT fooditemid FROM fooditem WHERE name = "Caesar Salad")
+    AND date LIKE "____-12-%";
+-- 6. View all establishments with a high average rating (rating >= 4). (ratings from 1-5; highest is5);
+    SELECT * FROM ratedfoodestablishment
+    WHERE average_rating >= 4;
+-- 7. View all food items from an establishment arranged according to price;
+    SELECT * FROM fooditem
+    WHERE foodestid = 4
+    ORDER BY price;
+-- 8. Search food items from any establishment based on a given price range and/or food type.
+    -- a. Price range only
+    SELECT *, GROUP_CONCAT(type) AS "Food types"
+    FROM fooditem AS i RIGHT JOIN foodtype AS t ON i.fooditemid = t.fooditemid
+    WHERE i.price >= 0 AND i.price <= 1000
+    GROUP BY name ORDER BY i.fooditemid;
+    -- b. Food type only
+    SELECT *, GROUP_CONCAT(type) AS "Food types"
+    FROM fooditem AS i RIGHT JOIN foodtype AS t ON i.fooditemid = t.fooditemid
+    WHERE t.type IN ("American", "Side Dish")
+    GROUP BY name ORDER BY i.fooditemid;
+    -- c. Price range and food type
+    SELECT *, GROUP_CONCAT(type) AS "Food types"
+    FROM fooditem AS i RIGHT JOIN foodtype AS t ON i.fooditemid = t.fooditemid
+    WHERE i.price >= 0 AND i.price <= 1000
+    AND t.type IN ("American", "Side Dish")
+    GROUP BY name ORDER BY i.fooditemid;
