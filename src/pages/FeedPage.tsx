a0537a1b9db1b422872ddd770207810d7be4b690
ico <icodevega@gmail.com>
2024-06-02 00:21:41 +0800

    feature: implemented  initial context provider for feed.

--- src/pages/FeedPage.tsx
+++ src/pages/FeedPage.tsx
@@ -7,6 +7,13 @@ import { EFEstablishmentCard } from "../components/estab_feed/EFEstablishmentCar
 import { FoodItemFeedFilterCard } from "../components/food_feed/FFFilterCard.tsx";
 import { FFFoodCard } from "../components/food_feed/FFFoodCard.tsx";
 import { EFExpandModal } from "../components/estab_feed/EFExpandModal.tsx";
+import { Establishment, sampleEstablishment } from "../models/Establishment.ts";
+import { sampleFoodItem } from "../models/FoodItem.ts";
+
+export const FeedContext = React.createContext({
+  modalEstablishment: sampleEstablishment,
+  setModalEstablishment: (establishment: Establishment) => {},
+});
 
 export function EstablishmentFeedPage() {
   // @ TODO: implement filter logic
@@ -15,108 +22,113 @@ export function EstablishmentFeedPage() {
   // toggle for showing establishment (false) or food items (true)
   const [toggle, setToggle] = useState(false);
 
+  // state to store the establishment to be displayed in the modal
+  const [modalEstablishment, setModalEstablishment] =
+    useState(sampleEstablishment); // temporary
+
   return (
-    <div className="flex flex-col items-center">
-      <EFExpandModal />
-      <NavigationBar />
-      {/* MAIN CONTAINER*/}
-      <div className="flex min-h-[86vh] w-full max-w-[1080px] flex-1 flex-col items-stretch p-8">
-        <div className="flex flex-row flex-wrap items-center justify-between px-4 pt-6 text-left text-3xl font-medium ">
-          <div className="line-clamp-1 h-min w-max flex flex-row items-center">
-            <span className="mr-3">
-              Browse {toggle ? " Food Items" : "Establishments"}
-            </span>
-          </div>
-          <div className="line-clamp-1 w-max text-base font-normal text-neutral">
-            {/* @TODO: make this conditional */}
-            {10 ? 10 + " results available." : "Fetching results..."}
-          </div>
-        </div>
-        <div className="flex h-max flex-row">
-          {/* MAIN SECTION */}
-          <main className="flex min-h-[100vh] max-w-[1080px] flex-[2] basis-[2fr] flex-col items-stretch bg-base-100 p-4 px-0 lg:basis-[3fr]">
-            <div
-              className={
-                "grid grid-cols-1 gap-6 lg:grid-cols-2 " +
-                (toggle ? "hidden" : "")
-              }
-            >
-              {/* @TODO: change this to a mapping function of fetched establishments */}
-              {true ? (
-                <>
-                  <EFEstablishmentCard />
-                  <EFEstablishmentCard />
-                  <EFEstablishmentCard />
-                  <EFEstablishmentCard />
-                  <EFEstablishmentCard />
-                </>
-              ) : (
-                // display mock cards to signify loading
-                <>
-                  <EFMockCard />
-                  <EFMockCard />
-                  <EFMockCard />
-                  <EFMockCard />
-                </>
-              )}
+    <FeedContext.Provider value={{ modalEstablishment, setModalEstablishment }}>
+      <div className="flex flex-col items-center">
+        <EFExpandModal />
+        <NavigationBar />
+        {/* MAIN CONTAINER*/}
+        <div className="flex min-h-[86vh] w-full max-w-[1080px] flex-1 flex-col items-stretch p-8">
+          <div className="flex flex-row flex-wrap items-center justify-between px-4 pt-6 text-left text-3xl font-medium ">
+            <div className="line-clamp-1 h-min w-max flex flex-row items-center">
+              <span className="mr-3">
+                Browse {toggle ? " Food Items" : "Establishments"}
+              </span>
             </div>
-            <div
-              className={
-                "grid grid-cols-1 gap-6 lg:grid-cols-2 " +
-                (!toggle ? "hidden" : "")
-              }
-            >
-              {/* @TODO: change this to a mapping function of fetched establishments */}
-              {true ? (
-                <>
-                  <FFFoodCard />
-                  <FFFoodCard />
-                </>
-              ) : (
-                // display mock cards to signify loading
-                <>
-                  <EFMockCard />
-                  <EFMockCard />
-                  <EFMockCard />
-                  <EFMockCard />
-                </>
-              )}
+            <div className="line-clamp-1 w-max text-base font-normal text-neutral">
+              {/* @TODO: make this conditional */}
+              {10 ? 10 + " results available." : "Fetching results..."}
             </div>
-          </main>
-          {/* (this section is hidden when screen is narrow (mobile)) */}
-          <aside className=" sticky top-[5.5rem] hidden h-max max-h-[90vh] flex-col self-start overflow-y-scroll rounded-xl pb-10 sm:flex">
-            <div className="card ml-4 mt-4">
-              <div className="join">
-                <button
-                  className={
-                    "join-item btn text-white text-xl " +
-                    (!toggle ? "bg-neutral hover:bg-neutral" : "")
-                  }
-                  onClick={() => setToggle(false)}
-                >
-                  Establishments
-                </button>
-                <button
-                  className={
-                    "join-item btn text-white text-xl " +
-                    (toggle ? "bg-neutral hover:bg-neutral" : "")
-                  }
-                  onClick={() => setToggle(true)}
-                >
-                  Food Items
-                </button>
+          </div>
+          <div className="flex h-max flex-row">
+            {/* MAIN SECTION */}
+            <main className="flex min-h-[100vh] max-w-[1080px] flex-[2] basis-[2fr] flex-col items-stretch bg-base-100 p-4 px-0 lg:basis-[3fr]">
+              <div
+                className={
+                  "grid grid-cols-1 gap-6 lg:grid-cols-2 " +
+                  (toggle ? "hidden" : "")
+                }
+              >
+                {/* @TODO: change this to a mapping function of fetched establishments */}
+                {true ? (
+                  <>
+                    <EFEstablishmentCard />
+                    <EFEstablishmentCard />
+                    <EFEstablishmentCard />
+                    <EFEstablishmentCard />
+                    <EFEstablishmentCard />
+                  </>
+                ) : (
+                  // display mock cards to signify loading
+                  <>
+                    <EFMockCard />
+                    <EFMockCard />
+                    <EFMockCard />
+                    <EFMockCard />
+                  </>
+                )}
               </div>
-            </div>
-            {/* Filter card */}
-            {toggle ? (
-              <FoodItemFeedFilterCard applyFilter={applyFilter} />
-            ) : (
-              <EstablishmentFeedFilterCard applyFilter={applyFilter} />
-            )}
-          </aside>
+              <div
+                className={
+                  "grid grid-cols-1 gap-6 lg:grid-cols-2 " +
+                  (!toggle ? "hidden" : "")
+                }
+              >
+                {/* @TODO: change this to a mapping function of fetched establishments */}
+                {true ? (
+                  <>
+                    <FFFoodCard foodItem={sampleFoodItem} />
+                  </>
+                ) : (
+                  // display mock cards to signify loading
+                  <>
+                    <EFMockCard />
+                    <EFMockCard />
+                    <EFMockCard />
+                    <EFMockCard />
+                  </>
+                )}
+              </div>
+            </main>
+            {/* (this section is hidden when screen is narrow (mobile)) */}
+            <aside className=" sticky top-[5.5rem] hidden h-max max-h-[90vh] flex-col self-start overflow-y-scroll rounded-xl pb-10 sm:flex">
+              <div className="card ml-4 mt-4">
+                <div className="join">
+                  <button
+                    className={
+                      "join-item btn text-white text-xl " +
+                      (!toggle ? "bg-neutral hover:bg-neutral" : "")
+                    }
+                    onClick={() => setToggle(false)}
+                  >
+                    Establishments
+                  </button>
+                  <button
+                    className={
+                      "join-item btn text-white text-xl " +
+                      (toggle ? "bg-neutral hover:bg-neutral" : "")
+                    }
+                    onClick={() => setToggle(true)}
+                  >
+                    Food Items
+                  </button>
+                </div>
+              </div>
+              {/* Filter card */}
+              {toggle ? (
+                <FoodItemFeedFilterCard applyFilter={applyFilter} />
+              ) : (
+                <EstablishmentFeedFilterCard applyFilter={applyFilter} />
+              )}
+            </aside>
+          </div>
         </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </FeedContext.Provider>
   );
 }
